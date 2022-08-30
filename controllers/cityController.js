9908df0378a9cda046b7ac36b836e3e15e4d4763
eugeniarossanigo <eugeniarossanigo@gmail.com>
2022-08-30 11:30:52 -0300

    controllers for methods and routes added

--- controllers/cityController.js
+++ controllers/cityController.js
@@ -1,7 +1,7 @@
 const City = require('../models/City')
 
 const cityController = {
-    create: async(req, res) => {
+    createCity: async(req, res) => {
         // const {city, country, photo, population, fundation, description} = req.body
         // req.body tiene todas las variables que necesito
         try {
@@ -17,7 +17,7 @@ const cityController = {
             })
         }
     },
-    read: async(req, res) => {
+    readCity: async(req, res) => {
         const {id} = req.params
         try {
             let city = await City.findOne({_id:id})
@@ -40,7 +40,74 @@ const cityController = {
                 success: false
             })
         }
+    },
+    readAll: async(req, res) => {
+        try {
+            let cities = await City.find()
+            if (cities) {
+                res.status(200).json({
+                    message: 'you get all the cities',
+                    response: cities,
+                    success: true
+                })
+            } else {
+                res.status(404).json({
+                    message: 'could not find any cities',
+                    success: false
+                })
+            }
+        } catch(error) {
+            console.log(error)
+            res.status(400).json({
+                message: "error",
+                success: false
+            })
+        }
+    },
+    deleteCity: async(req, res) => {
+        const {id} = req.params
+        let city
+        try {
+            city = await City.findOneAndDelete({_id:id})
+            res.status(200).json({
+                message: 'you have removed the city',
+                response: city,
+                success: true
+            })
+        } catch(error) {
+            res.status(404).json({
+                message: "could not delete the city",
+                success: false
+            })
+        }
+    },
+    updateCity: async(req, res) => {
+        const {id} = req.params
+        const city = req.body.data
+        try {
+            let newCity = await City.findOneAndUpdate({_id:id}, city, {new: true})
+            res.status(200).json({
+                message: 'you have removed the city',
+                response: newCity,
+                success: true
+            })
+        }  catch(error) {
+            res.status(400).json({
+                message: "could not update the city",
+                success: false
+            })
+        }
     }
 }
 
 module.exports = cityController
+
+
+// {
+//     "city": "Rosario",
+//     "country": "Argentina",
+//     "photo": "asdasdasdsad.jpg",
+//     "population": 1000000,
+//     "fundation": "1950-12-12",
+//     "description": "hola"
+//     }
