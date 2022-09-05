b161e831e2ec0272670a2a6ad0dbf684c7aa8a98
AmuchasteguiMarcos317 <amuchasteguimarcos317@gmail.com>
2022-09-05 12:54:52 -0300

    defined the controller and route to modify an itinerary

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const { createItinerary } = require ('../controllers/itineraryController')
+const { createItinerary,updateItinerary } = require ('../controllers/itineraryController')
 
 router.post('/' , createItinerary)
+router.put('/:id', updateItinerary)
 
 module.exports = router;
