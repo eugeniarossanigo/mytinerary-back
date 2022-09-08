74e9b8e30894f5dcf08dee9186350ee76330b637
AmuchasteguiMarcos317 <amuchasteguimarcos317@gmail.com>
2022-09-08 02:27:23 -0300

    test cities and itineraries

---
+++ test/itinerariesTest.js
@@ -0,0 +1,32 @@
+const request = require('supertest')
+const app = require('../app')
+const { assert } = require('chai')
+
+describe('POST /itineraries', function () {
+    it('Must respond with 201 status code', function (done) {
+        request(app)
+            .post('/itineraries')
+            .send({
+                name: 'Test Itinerary',
+                user: '6318aff24a57ccc17393d91a',
+                city: '631768d0c561b3f4a98d5534',
+                price: 7000,
+                likes: [7],
+                tags: ['test'],
+                duration: 7
+            })
+            .expect(201, done)
+    })
+})
+
+describe('DELETE /itineraries', function () {
+    id = "63197bbbcb6d35a00e73e411"
+    it('Must detele itinerary', function (done) {
+        request(app)
+            .del('/itineraries/'+id)
+            .expect(200, done)
+            })
+    })
+
+
+
