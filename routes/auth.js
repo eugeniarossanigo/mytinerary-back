f97aba741308969f9a6318ce378c398a1f6304dd
eugeniarossanigo <eugeniarossanigo@gmail.com>
2022-09-15 16:45:33 -0300

    deleted code fixed

---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+const { signUp, verifyMail, signIn, signOut, readUsers } = require ('../controllers/userController')
+
+router.post('/signup', signUp);
+router.post('/signin', signIn);
+router.get('/verify/:code', verifyMail);
+router.post('/signout', signOut);
+router.get('/', readUsers)
+
+module.exports = router;
