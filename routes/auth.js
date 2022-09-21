b723009255465fb0f9f223c677aed681acc1a1f0
eugeniarossanigo <eugeniarossanigo@gmail.com>
2022-09-21 15:10:46 -0300

    route added

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const { signUp, verifyMail, signIn, signOut, readUsers } = require ('../controllers/userController')
+const { signUp, verifyMail, signIn, signInToken, signOut, readUsers } = require ('../controllers/userController')
 
 router.post('/signup', signUp);
 router.post('/signin', signIn);
+router.get('/token', passport.authenticate('jwt', {session:false}), signInToken);
 router.get('/verify/:code', verifyMail);
 router.post('/signout', signOut);
-router.get('/', readUsers)
+router.get('/', readUsers);
 
 module.exports = router;
