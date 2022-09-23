7d675b1f1cc38ca7ce92c26793593f4391dfbbba
eugeniarossanigo <eugeniarossanigo@gmail.com>
2022-09-23 11:29:56 -0300

    readitinerary controller added

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 let passport = require('../config/passport')
 
-const { createItinerary, updateItinerary, deleteItinerary, readAllItineraries, likeDislike} = require ('../controllers/itineraryController')
+const { createItinerary, updateItinerary, deleteItinerary, readAllItineraries, readItinerary, likeDislike} = require ('../controllers/itineraryController')
 
 router.post('/', createItinerary)
 router.get('/', readAllItineraries)
+router.get('/:id', readItinerary)
 router.patch('/:id', updateItinerary)
 router.delete('/:id', deleteItinerary)
 router.patch('/likes/:id', passport.authenticate('jwt', {session:false}), likeDislike)
